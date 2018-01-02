870e44afeaad79535125543ba2f048b319e8b95d
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-02 11:55:36 -0700

    heroku changes

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
-
+require('dotenv').config()
+const environment = process.env.NODE_ENV || 'development'
 module.exports = {
 
   development: {
@@ -10,36 +11,10 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+    
   }
 
 };
