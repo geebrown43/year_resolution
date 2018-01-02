e0fa08156ae4947bb0445a2732f4572618201381
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-02 13:44:38 -0700

    added cors middleware

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const index = require('./routes/year_resolution')
-
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/', index)
+
 
 app.listen(port, () => {
     console.log('Listening on port:' + port)
